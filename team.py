ca23223feb918b02d730e4c56d3f43423378d931
GUZEEWON <pink7519@pusan.ac.kr>
2022-01-21 01:14:10 +0900

    number game commit

--- team.py
+++ team.py
@@ -1,17 +1,20 @@
-print("★ 구구단을 출력합니다.\n")
-for x in range(2, 10):
-    print("------- [" + str(x) + "단] -------")
-    for y in range(1, 10):
-        print(x, "X", y, "=", x*y)
-print("---------------------")
+# 숫자 up&down game
+import random
+# game을 위한 random 숫자 생성
+rn = random.randrange(1,101,1)
+num = -1
 
-print("I like python")
-
-def print_sum():
-    a =100
-    b= 200
-    result=a+b
-    print('print_sum() 내부 :',a,'과',b,'의 합은',result,'입니다')
-a=20
-b=30
-print_sum()
+t_cnt = 0 #시도 횟수
+print("1~100 숫자 up&down 게임을 시작합니다!")
+print("-------------------------------------")
+while(rn != num):
+    
+    num=int(input("1부터 100사이의 숫자를 입력하세요."))
+    if( num> rn):
+        print("Down")
+    elif(num < rn):
+        print("Up")
+    
+    t_cnt+=1
+print("----------------------------------------")
+print(t_cnt,"번 만에 정답!")

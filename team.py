f97887e0cd58b98b29b8920cbe97dcbd499d132c
GUZEEWON <pink7519@pusan.ac.kr>
2022-01-20 20:02:10 +0900

    freshman first commit

--- team.py
+++ team.py
@@ -4,3 +4,14 @@ for x in range(2, 10):
     for y in range(1, 10):
         print(x, "X", y, "=", x*y)
 print("---------------------")
+
+print("I like python")
+
+def print_sum():
+    a =100
+    b= 200
+    result=a+b
+    print('print_sum() 내부 :',a,'과',b,'의 합은',result,'입니다')
+a=10
+b=20
+print_sum()
